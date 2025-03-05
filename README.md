a2ce6703b4f069995caf18b5ff963cff4db0015a
Limbo <yykaue@qq.com>
2025-03-05 11:07:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## limbo-common-css
+
+   通用样式库
+
+## 安装方法
+
+    npm i limbo-common-css -s
+
+
+## 使用说明
---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ *Created by limbo <yykaue@qq.com> on 2019/7/3.
+ */
+import index from './lib/index/index.scss'
+import fontCrm from './lib/iconfont/iconfont.css'
+import iconfont from './lib/iconfont/iconfont'
+
+export {
+  index,
+  fontCrm,
+  iconfont
+}
